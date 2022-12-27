0c6e8ea069b11e5e6dcb796bccebaa6bc3ea9ced
rizky ferdian <rizkyferdian002@gmail.com>
2022-12-27 22:10:13 +0700

    Finishing

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const expressLayouts = require('express-ejs-layouts')
 const session = require('express-session')
 const flash = require('connect-flash')
 const mongoose = require('mongoose')
+const { body, validationResult, check, Result } = require('express-validator')
+const methodOverride = require('method-override')
 
 const Contact = require('./model/contact')
+const { findOne } = require('./model/contact')
+const { render } = require('ejs')
 
 
 const app = express()
 const port = 3000
+
+app.use(methodOverride('_method'))
 
 mongoose
     .connect('mongodb://127.0.0.1:27017/belajar_nodejs', {})
@@ -55,6 +61,97 @@ app.get('/contact', async (req, res) => {
         contacts
     })
 })
+
+app.get('/contact/add', (req, res) => {
+    res.render('add-contact', {
+        layout: 'layouts/main-layouts',
+    })
+})
+
+
+
+app.post('/contact', [
+    body('nama').custom(async (value) => {
+        const duplikat = await Contact.findOne({ nama: value })
+        if (duplikat) {
+            throw new Error('Nama Sudah Tersedia')
+        }
+        return true
+    }),
+    check('email', 'Email tidak valid').isEmail(),
+    check('nohp', 'No HP tidak valid').isMobilePhone()
+], (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        res.render('add-contact', {
+            layout: 'layouts/main-layouts',
+            errors: errors.array()
+        })
+    } else {
+        Contact.insertMany(req.body, (error, result) => {
+            res.redirect('/contact')
+        })
+    }
+});
+
+app.delete('/contact', (req, res) => {
+    Contact.deleteOne({ nama: req.body.nama }).then((result) => {
+        res.redirect('/contact')
+    })
+})
+
+// app.get('/contact/delete/:nama', async (req, res) => {
+//     const contact = await Contact.findOne({ nama: req.params.nama })
+//     if (!contact) {
+//         res.send('<h1> 404 </h1>')
+//     } else {
+//         Contact.deleteOne({ _id: contact._id }).then((result) => {
+//             res.redirect('/contact')
+//         })
+//     }
+// })
+
+app.get('/contact/edit/:nama', async (req, res) => {
+    const contact = await Contact.findOne({ nama: req.params.nama })
+    res.render('edit-contact', {
+        layout: 'layouts/main-layouts',
+        contact
+    })
+})
+
+app.put('/contact', [
+    body('nama').custom(async (value, { req }) => {
+        const duplikat = await Contact.findOne({ nama: value })
+        if (!value !== req.body.oldNama && duplikat) {
+            throw new Error('Nama sudah digunakan !')
+        }
+        return true
+    }),
+    check('email', 'email tidak valid').isEmail(),
+    check('nohp', 'No Hp tidak valid').isMobilePhone(),
+], (req, res) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+        res.render('edit-contact', {
+            layout: 'layouts/main-layouts',
+            error: error.array(),
+            contact: req.body
+        })
+    } else {
+        Contact.updateOne({ _id: req.body._id }, {
+            $set: {
+                nama: req.body.nama,
+                email: req.body.email,
+                nohp: req.body.nohp
+            }
+        }
+        ).then((result) => {
+            res.redirect('/contact')
+        })
+    }
+}
+
+)
 
 app.get('/contact/:nama', async (req, res) => {
     const contact = await Contact.findOne({ nama: req.params.nama })
